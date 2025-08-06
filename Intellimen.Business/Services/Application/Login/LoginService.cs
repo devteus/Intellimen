10020d83de6ac8fd0557cb9daed9bc59bc0cd230
devteus <miongamae@gmail.com>
2025-08-06 06:14:23 -0300

    [FEATURE] Starting implementation of the login method

---
+++ Intellimen.Business/Services/Application/Login/LoginService.cs
@@ -0,0 +1,11 @@
+﻿using Intellimen.Repository.Context;
+
+namespace Intellimen.Business.Services.Application.Login
+{
+    public class LoginService(IntellimenDbContext context)
+    {
+        private readonly IntellimenDbContext _intellimenDbContext = context;
+
+
+    }
+}
---
+++ Intellimen.Business/Services/Application/Login/LoginValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Intellimen.Business.Requests;
+
+namespace Intellimen.Business.Services.Application.Login
+{
+    public class LoginValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email é obrigatório")
+                .EmailAddress().WithMessage("Formato de email inválido");
+        }
+    }
+}
